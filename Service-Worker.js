d3ca2487342847d26dbff587267c79709897def1
1gdiegovillacorta <100307767+1gdiegovillacorta@users.noreply.github.com>
2022-06-14 18:17:12 -0600

    1st Commit

---
+++ Service-Worker.js
@@ -0,0 +1,28 @@
+//JavaScript
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll([
+                "./index.html",
+                "./styles.css",
+                "./manifest.json",
+                "./Register.js",
+                "./Images/Pic1.jpg",
+                "./Images/Pic2.jpg",
+                "./Images/Pic3.jpg",
+                "./Images/App-Icons/App-Icon-192.png",
+                "./Images/Icon.svg",
+                "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
