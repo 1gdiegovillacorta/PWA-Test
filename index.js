51af75a34d1e2c3980e1b7a2fe06fc14ce41ea62
1gdiegovillacorta <100307767+1gdiegovillacorta@users.noreply.github.com>
2022-06-19 22:18:26 -0600

    13th Commit

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ let BookCover = document.getElementById("Book-Cover");
 function OpenPic2() {
     DescriptionBox.style.height = "100%"
     BookTitle.innerHTML = "The Invisible Man";
+    //BookTitle.setAttribute("class", "Book-Title animate__bounceIn");
+    BookCover.setAttribute("class", "Book-Cover animate__bounceIn");
     BookCover.setAttribute("src", "./Images/The-Invisible-Man-Book-Cover.jpg");
+    document.getElementById("Read-Button").setAttribute("class", "Read-Button animate__bounceIn");
 }
 
 function CloseDescriptionBox() {
     DescriptionBox.style.height = "0%";
+    //BookTitle.setAttribute("class", "");
+    BookCover.setAttribute("src", "");
+    BookCover.setAttribute("class", "");
+    document.getElementById("Read-Button").setAttribute("class", "Read-Button");
 }
